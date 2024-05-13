e5bc679c044d628d35e0dd930243e56467793b53
Abel Ciccoli <abelcoli@outlook.fr>
2024-05-13 12:41:48 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ColocScopie',
+  webDir: 'www'
+};
+
+export default config;
